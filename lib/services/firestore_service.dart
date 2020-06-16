2b1d045fa670bea01fb4b7dc510b3a310bea6991
aryanvichare <aryanvichare10@gmail.com>
2020-06-16 15:24:32 -0700

    add additional student user data

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,12 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 
 class FirestoreService {
   final CollectionReference users = Firestore.instance.collection("users");
 
-  Future<void> updateUserData(
-      {String uid, String email, String fullName, String type}) async {
+  Future<void> updateStudentUserData(
+      {@required String uid,
+      @required String email,
+      @required String firstName,
+      @required String lastName,
+      @required String grade,
+      @required List activeProjects,
+      @required List pendingProjects,
+      @required Map<String, String> reviewProjects,
+      @required String userType}) async {
     return await users.document(uid).setData(
-      {'uid': uid, 'fullName': fullName, 'email': email, 'type': type},
+      {
+        'uid': uid,
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': email,
+        'grade': grade,
+        'activeProjects': activeProjects,
+        'reviewProjects': reviewProjects,
+        'pendingProjects': pendingProjects,
+        'userType': userType
+      },
+    );
+  }
+
+  Future<void> updateUserData(
+      {String uid, String email, String fullName, String userType}) async {
+    return await users.document(uid).setData(
+      {'uid': uid, 'fullName': fullName, 'email': email, 'userType': userType},
     );
   }
 
