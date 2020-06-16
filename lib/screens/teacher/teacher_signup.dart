018e25eff3a6002902091d1da71310958a3d04f8
aryanvichare <aryanvichare10@gmail.com>
2020-06-16 15:53:32 -0700

    add fields for preferred prefix, grade, and school

--- lib/screens/teacher/teacher_signup.dart
+++ lib/screens/teacher/teacher_signup.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:remoteta_app/components/rounded_button.dart';
-import 'package:remoteta_app/screens/student/student_dashboard.dart';
-import 'package:remoteta_app/screens/student/student_login.dart';
+import 'package:remoteta_app/screens/teacher/teacher_login.dart';
 import '../../constants/constants.dart';
 import 'package:provider/provider.dart';
 import 'package:remoteta_app/services/firebase_auth_service.dart';
@@ -16,12 +15,52 @@ class TeacherSignUpScreen extends StatefulWidget {
 
 class _TeacherSignUpScreenState extends State<TeacherSignUpScreen> {
   final key = GlobalKey<FormState>();
-  String fullName, email, password, confirmationPassword;
+  String fullName, prefix, email, password, confirmationPassword, grade, school;
+  List<String> _gradeLevels = ['9th', '10th', '11th', '12th'];
+  List<DropdownMenuItem<String>> _gradeItems;
+  String _selectedGrade;
+
+  List<String> _prefixes = ['Ms', 'Mrs', 'Mr'];
+  List<DropdownMenuItem<String>> _prefixItems;
+  String _selectedPrefix;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _selectedGrade = _gradeLevels[0];
+    _gradeItems = _buildDropdownMenuItems(_gradeLevels);
+
+    _selectedPrefix = _prefixes[0];
+    _prefixItems = _buildDropdownMenuItems(_prefixes);
+  }
+
+  List<DropdownMenuItem<String>> _buildDropdownMenuItems(
+      List<String> listItems) {
+    List<DropdownMenuItem<String>> items = List();
+    for (int i = 0; i < listItems.length; i++) {
+      items.add(
+        new DropdownMenuItem(
+          value: listItems[i],
+          child: Text(listItems[i]),
+        ),
+      );
+    }
+    return items;
+  }
+
+  void onChangeDropdownGradeItem(String s) {
+    setState(() {
+      _selectedGrade = s;
+      grade = _selectedGrade;
+    });
+  }
+
+  void onChangeDropdownPrefixItem(String s) {
+    setState(() {
+      _selectedPrefix = s;
+      prefix = _selectedPrefix;
+    });
   }
 
   Future<void> createUserWithEmailAndPassword(BuildContext context) async {
@@ -40,10 +79,19 @@ class _TeacherSignUpScreenState extends State<TeacherSignUpScreen> {
         }
 
         final firestore = Provider.of<FirestoreService>(context, listen: false);
-        firestore.updateUserData(
+        List nameItems = this.fullName.trim().split(" ");
+
+        firestore.updateTeacherUserData(
           uid: user.uid,
           email: this.email,
-          fullName: this.fullName,
+          firstName: nameItems[0],
+          lastName: nameItems[1],
+          preferredPrefix: prefix,
+          activeProjects: [],
+          pendingProjects: [],
+          reviewProjects: {},
+          grade: grade,
+          school: school,
           userType: 'teacher',
         );
 
@@ -86,7 +134,7 @@ class _TeacherSignUpScreenState extends State<TeacherSignUpScreen> {
         return AlertDialog(
           title: new Text("Email Confirmation Verification"),
           content: new Text(
-              "An email was sent to ${email}. Please check your email to get verified into RemoteTA"),
+              "An email was sent to $email. Please check your email to get verified into RemoteTA"),
           actions: <Widget>[
             // usually buttons at the bottom of the dialog
             new FlatButton(
@@ -95,7 +143,7 @@ class _TeacherSignUpScreenState extends State<TeacherSignUpScreen> {
                 Navigator.push(
                   context,
                   new MaterialPageRoute(
-                    builder: (context) => TeacherSignUpScreen(),
+                    builder: (context) => TeacherLoginScreen(),
                   ),
                 );
               },
@@ -212,7 +260,7 @@ class _TeacherSignUpScreenState extends State<TeacherSignUpScreen> {
                       fullName = value;
                     },
                     decoration: kInputDecoration.copyWith(
-                      hintText: 'Full Name',
+                      hintText: 'First & Last Name',
                       prefixIcon: Icon(
                         Icons.person,
                         color: kInputOutlineColor,
@@ -223,7 +271,30 @@ class _TeacherSignUpScreenState extends State<TeacherSignUpScreen> {
                   TextFormField(
                     validator: (val) {
                       if (val.isEmpty) {
-                        return 'Please enter your email';
+                        return 'Please enter your school email';
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      school = value;
+                    },
+                    decoration: kInputDecoration.copyWith(
+                      hintText: 'Enter your school email',
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: kInputOutlineColor,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextFormField(
+                    validator: (val) {
+                      if (val.isEmpty) {
+                        return 'Please enter the school you are teaching at';
                       }
                       return null;
                     },
@@ -233,7 +304,7 @@ class _TeacherSignUpScreenState extends State<TeacherSignUpScreen> {
                       email = value;
                     },
                     decoration: kInputDecoration.copyWith(
-                      hintText: 'Enter your email',
+                      hintText: 'Enter your school',
                       prefixIcon: Icon(
                         Icons.email,
                         color: kInputOutlineColor,
@@ -289,6 +360,41 @@ class _TeacherSignUpScreenState extends State<TeacherSignUpScreen> {
                         color: kInputOutlineColor,
                       ),
                     ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Text("Grade: "),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          DropdownButton(
+                            value: _selectedGrade,
+                            items: _gradeItems,
+                            onChanged: onChangeDropdownGradeItem,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Preferred Prefix: "),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      DropdownButton(
+                        value: _selectedPrefix,
+                        items: _prefixItems,
+                        onChanged: onChangeDropdownPrefixItem,
+                      ),
+                    ],
                   ),
                   SizedBox(
                     height: 8.0,
